fce5d240c1fb1d6d125074f595cf8416192aac88
lgrosu <lucian.grosu@endava.com>
2025-01-06 19:18:46 +0200

    WebTable

---
+++ src/test/java/Tests/WebTable.java
@@ -0,0 +1,67 @@
+package Tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class WebTable {
+
+    public WebDriver driver;
+
+    @Test
+    public void automationMethod() {
+        //deschidem un browser de Chrome
+        driver = new ChromeDriver();
+
+        //accesam o pagina web
+        driver.get("https://demoqa.com/");
+
+        //browser in modul maximize
+        driver.manage().window().maximize();
+
+        //facem un scroll
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,400)");
+
+        //declaram un element
+        WebElement ElementsField = driver.findElement(By.xpath("//h5[text()='Elements']"));
+        ElementsField.click();
+
+        WebElement TableField = driver.findElement(By.xpath("//span[text()='Web Tables']"));
+        TableField.click();
+
+        WebElement AddField = driver.findElement(By.id("addNewRecordButton"));
+        AddField.click();
+
+        WebElement FirstNameField = driver.findElement(By.id("firstName"));
+        String firstNameValue = "Lucian";
+        FirstNameField.sendKeys(firstNameValue);
+
+        WebElement LastNameField = driver.findElement(By.id("lastName"));
+        String lastNameValue = "Grosu";
+        LastNameField.sendKeys(lastNameValue);
+
+        WebElement emailField = driver.findElement(By.id("userEmail"));
+        String emailValue = "lucian.grosu@gmail.com";
+        emailField.sendKeys(emailValue);
+
+        WebElement ageField = driver.findElement(By.id("age"));
+        String ageValue = "43";
+        ageField.sendKeys(ageValue);
+
+        WebElement salaryField = driver.findElement(By.id("salary"));
+        String salaryValue = "12000";
+        salaryField.sendKeys(salaryValue);
+
+        WebElement departmentField = driver.findElement(By.id("department"));
+        String departmentValue = "IT";
+        departmentField.sendKeys(departmentValue);
+
+        WebElement SubmitField = driver.findElement(By.id("submit"));
+        SubmitField.click();
+    }
+
+}
