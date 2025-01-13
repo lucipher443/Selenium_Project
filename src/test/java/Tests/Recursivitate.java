11ec3024db6b91728f794033af8d7a9810831b9c
lgrosu <lucian.grosu@endava.com>
2025-01-13 16:48:48 +0200

    Recursivitate

---
+++ src/test/java/Tests/Recursivitate.java
@@ -0,0 +1,44 @@
+package Tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+
+public class Recursivitate {
+
+    public WebDriver driver;
+
+    @Test
+    public void parcurgereListe()
+    {
+        driver = new ChromeDriver();
+        driver.get("https://demoqa.com/");
+        driver.manage().window().maximize();
+        Actions actions = new Actions(driver);
+
+        List<WebElement> list = driver.findElements(By.xpath("//div[@id = 'demo-tabpane-list']//div[@class = 'list-group-item list-group-item-action']"));
+        for(int i = 0; i < list.size()-1; i++)
+        {
+            WebElement webElement = list.get(i);
+            WebElement webElement2 = list.get(i++);
+            System.out.println("Numarul elementului este " + webElement.getText());
+            actions.clickAndHold(webElement)
+                    .moveToElement(webElement2)
+                    .release()
+                    .build()
+                    .perform();
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
